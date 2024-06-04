6a44ea33ff1111a348f223919f7e2385fa282c2f
zalayetha <zalayetha@gmail.com>
2024-06-04 21:58:17 +0700

    create several component and start to slicing page home

--- app/src/main/java/com/capstone/yukonek/home/Home.kt
+++ app/src/main/java/com/capstone/yukonek/home/Home.kt
@@ -1,17 +1,60 @@
 package com.capstone.yukonek.home
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.capstone.yukonek.R
+import com.example.compose.ColorFamily
 import com.example.compose.YuKonekTheme
+import com.example.compose.onPrimaryLight
+import com.example.compose.primaryLight
+import com.example.ui.theme.AppTypography
+import com.google.android.material.color.MaterialColors
 
 class Home : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,26 +62,273 @@ class Home : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             YuKonekTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainView()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainView() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.primary
+    ) { innerPadding ->
 
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+
+            ) {
+                CardDisplayName()
+                // Content area (Placeholder for main content)
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                        .clip(shape = RoundedCornerShape(20.dp, 20.dp, 0.dp, 0.dp))
+                        .background(MaterialTheme.colorScheme.surface),
+                ) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp, vertical = 24.dp)
+                    ) {
+                        item {
+                            //                        Youtuber Recommendations
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text(
+                                    "YouTubers For You",
+                                    style = MaterialTheme.typography.titleMedium,
+                                    color = MaterialTheme.colorScheme.primary
+                                )
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        Icons.Filled.ArrowForward,
+                                        contentDescription = "View All",
+                                        tint = MaterialTheme.colorScheme.primary
+                                    )
+                                }
+                            }
+                            LazyRow(modifier = Modifier.fillMaxWidth()) {
+                                items(5) {
+                                    CardListYoutuber()
+                                    Spacer(modifier = Modifier.width(12.dp))
+                                }
+                            }
+
+                            Spacer(modifier = Modifier.height(24.dp))
+
+//                        Favorite Youtubers
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text(
+                                    "Your FavTubers",
+                                    style = MaterialTheme.typography.titleMedium,
+                                    color = MaterialTheme.colorScheme.primary
+                                )
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        Icons.Filled.ArrowForward,
+                                        contentDescription = "View All",
+                                        tint = MaterialTheme.colorScheme.primary
+                                    )
+                                }
+                            }
+                            LazyRow(modifier = Modifier.fillMaxWidth()) {
+                                items(5) {
+                                    CardListYoutuber()
+                                    Spacer(modifier = Modifier.width(12.dp))
+                                }
+                            }
+
+                            Spacer(modifier = Modifier.height(24.dp))
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text(
+                                    "Entertainment News",
+                                    style = MaterialTheme.typography.titleMedium,
+                                    color = MaterialTheme.colorScheme.primary
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                        // News
+                        items(10){
+                            CardEntertainmentNews()
+                            Spacer(modifier = Modifier.height(24.dp))
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
+fun CardDisplayName() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 24.dp, horizontal = 16.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Foto Profil",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape),
+            )
+
+            Column(modifier = Modifier.padding(horizontal = 0.dp)) {
+                Text(
+                    "Hi,Zaghy Zalayetha",
+                    style = AppTypography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+                Text(
+                    "Welcome back",
+                    style = AppTypography.bodySmall,
+                    color = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f)
+                )
+            }
+        }
+        Image(
+            painter = painterResource(id = R.drawable.ic_category_video_games),
+            contentDescription = "Foto Profil",
+            colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.onPrimary),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .size(32.dp)
+
+        )
+    }
+}
+
+@Composable
+fun CardListYoutuber() {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.thumbnail),
+                contentDescription = "Thumbnail youtuber",
+                modifier = Modifier.size(156.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text("Jerome Polin", style = AppTypography.titleSmall)
+            Spacer(modifier = Modifier.height(8.dp))
+            Card(
+                colors = CardColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                    disabledContainerColor = MaterialTheme.colorScheme.secondary,
+                    disabledContentColor = MaterialTheme.colorScheme.onSecondary
+                ), shape = RoundedCornerShape(20)
+            ) {
+                Text(
+                    "Education",
+                    style = AppTypography.labelMedium,
+                    modifier = Modifier.padding(12.dp)
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun CardEntertainmentNews() {
+    Card(colors = CardColors(
+        containerColor = MaterialTheme.colorScheme.surface,
+        contentColor = MaterialTheme.colorScheme.primary,
+        disabledContainerColor = MaterialTheme.colorScheme.surface,
+        disabledContentColor = MaterialTheme.colorScheme.secondary
+    )) {
+        Image(
+            painter = painterResource(id = R.drawable.thumbnail_news),
+            contentDescription = "Thumbnail News",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(211.dp),
+            contentScale = ContentScale.FillWidth
+
+
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Jess dan SIca Kohl Bucin Masuk Video Populer YouTube Indonesia 2022",
+            style = AppTypography.titleMedium
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            "Video kebucinan Jess No Limit bersama Sisca Kohl yang kini resmi menjadi istrinya menjadi salah satu video terpopuler di YouTube Indonesia pada 2022. Dalam video itu, pasangan.",
+            style = AppTypography.bodyMedium
+        )
+    }
+}
+
+
+@Preview(widthDp = 412, showBackground = true, backgroundColor = 0xFF395790)
+@Composable
+fun PreviewCardDisplayName() {
     YuKonekTheme {
-        Greeting("Android")
+        CardDisplayName()
+
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardListYoutuber() {
+    YuKonekTheme {
+        CardListYoutuber()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardEntertainmentNews() {
+    YuKonekTheme {
+        CardEntertainmentNews()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMainView() {
+    YuKonekTheme {
+        MainView()
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewMainViewDark() {
+    YuKonekTheme {
+        MainView()
     }
 }
