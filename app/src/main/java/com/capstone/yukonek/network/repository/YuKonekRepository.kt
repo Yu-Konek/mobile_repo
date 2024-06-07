433ad9284824d1e45bcd85598764d1fee2325417
zalayetha <zalayetha@gmail.com>
2024-06-07 10:54:28 +0700

    setup dependency injection

--- app/src/main/java/com/capstone/yukonek/network/repository/YuKonekRepository.kt
+++ app/src/main/java/com/capstone/yukonek/network/repository/YuKonekRepository.kt
@@ -1,4 +1,25 @@
 package com.capstone.yukonek.network.repository
 
-class YuKonekRepository {
+import com.capstone.yukonek.local.datastore.SettingPreferencesDataStore
+import com.capstone.yukonek.network.retrofit.ApiService
+
+class YuKonekRepository private constructor(
+    private val apiService: ApiService,
+    private val pref: SettingPreferencesDataStore
+) {
+
+    companion object {
+        @Volatile
+        private var instance: YuKonekRepository? = null
+        fun getInstance(
+            apiService: ApiService,
+            pref: SettingPreferencesDataStore,
+        ): YuKonekRepository = instance ?: synchronized(this) {
+            instance ?: YuKonekRepository(apiService, pref).also {
+                instance = it
+            }
+        }
+
+        private const val TAG = "YuKonekRepository"
+    }
 }
