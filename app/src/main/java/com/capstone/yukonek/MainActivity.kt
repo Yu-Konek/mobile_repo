f0c7c85a336509e81dffe7dffb574430d6c799ed
zalayetha <zalayetha@gmail.com>
2024-06-08 09:06:04 +0700

    fix-dependencies

--- app/src/main/java/com/capstone/yukonek/MainActivity.kt
+++ app/src/main/java/com/capstone/yukonek/MainActivity.kt
@@ -5,14 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.capstone.yukonek.navigations.AppNavHost
-import com.example.compose.YuKonekTheme
+import com.capstone.yukonek.ui.theme.YuKonekTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
