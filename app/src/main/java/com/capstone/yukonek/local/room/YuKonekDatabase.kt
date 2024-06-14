37996f029f903c52ae3d63c186aad90f10e01a31
Charish128 <charis12876@gmail.com>
2024-06-14 20:21:29 +0700

    Adding YuKonekDatabase.kt

--- app/src/main/java/com/capstone/yukonek/local/room/YuKonekDatabase.kt
+++ app/src/main/java/com/capstone/yukonek/local/room/YuKonekDatabase.kt
@@ -4,24 +4,26 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.capstone.yukonek.home.data.TodoItem
 
-//@Database(version = 2, exportSchema = false)
-//abstract class YuKonekDatabase : RoomDatabase(){
-//    companion object{
-//        @Volatile
-//        private var INSTANCE: YuKonekDatabase? = null
-//        @JvmStatic
-//        fun getInstance(context: Context): YuKonekDatabase {
-//            return INSTANCE ?: synchronized(this) {
-//                INSTANCE ?: Room.databaseBuilder(
-//                    context.applicationContext,
-//                    YuKonekDatabase::class.java, "yu_konek_database"
-//                )
-//                    .fallbackToDestructiveMigration()
-//                    .build()
-//                    .also { INSTANCE = it }
-//
-//            }
-//        }
-//    }
-//}
+@Database(entities = [TodoItem::class], version = 2, exportSchema = false)
+abstract class YuKonekDatabase : RoomDatabase(){
+    abstract fun todoDao(): TodoDao
+    companion object{
+        @Volatile
+        private var INSTANCE: YuKonekDatabase? = null
+        @JvmStatic
+        fun getInstance(context: Context): YuKonekDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    YuKonekDatabase::class.java, "yu_konek_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/capstone/yukonek/network/repository/YuKonekRepository.kt
+++ app/src/main/java/com/capstone/yukonek/network/repository/YuKonekRepository.kt
@@ -4,20 +4,25 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.liveData
 import com.capstone.yukonek.home.data.MResponseNews
+import com.capstone.yukonek.home.data.TodoItem
 import com.capstone.yukonek.local.datastore.SettingPreferencesDataStore
+import com.capstone.yukonek.local.room.YuKonekDatabase
 import com.capstone.yukonek.network.Result
 import com.capstone.yukonek.network.error.ErrorResponse
 import com.capstone.yukonek.network.retrofit.myapi.PrivateApiService
 import com.capstone.yukonek.network.retrofit.newsapi.NewsApiService
 import com.google.gson.Gson
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.toCollection
 import retrofit2.HttpException
 
 class YuKonekRepository private constructor(
     private val apiService: PrivateApiService,
     private val newsApiService: NewsApiService,
     private val pref: SettingPreferencesDataStore,
-    private val savedStateHandle: SavedStateHandle
+    private val savedStateHandle: SavedStateHandle,
+    private val database: YuKonekDatabase
 ) {
     fun getTheme(): Flow<Boolean> {
         return pref.getThemeSetting()
@@ -43,6 +48,22 @@ class YuKonekRepository private constructor(
 
     fun getArguments(arg: String): Flow<String> = checkNotNull(savedStateHandle[arg])
 
+    fun getAllTodoItems(): Flow<List<TodoItem>> {
+        return database.todoDao().getAllTodoItem()
+    }
+
+    suspend fun insertTodoItems(todoItem: TodoItem) {
+        database.todoDao().insertTodoItem(todoItem)
+    }
+
+    suspend fun deleteTodoItems(todoItem: TodoItem) {
+        database.todoDao().deleteTodoItem(todoItem)
+    }
+
+    suspend fun updateTodoItems(todoItem: TodoItem) {
+        database.todoDao().updateTodoItem(todoItem)
+    }
+
 
     companion object {
         @Volatile
@@ -51,9 +72,16 @@ class YuKonekRepository private constructor(
             apiService: PrivateApiService,
             newsApiService: NewsApiService,
             pref: SettingPreferencesDataStore,
-            savedStateHandle: SavedStateHandle
+            savedStateHandle: SavedStateHandle,
+            database: YuKonekDatabase
         ): YuKonekRepository = instance ?: synchronized(this) {
-            instance ?: YuKonekRepository(apiService, newsApiService, pref, savedStateHandle).also {
+            instance ?: YuKonekRepository(
+                apiService,
+                newsApiService,
+                pref,
+                savedStateHandle,
+                database
+            ).also {
                 instance = it
             }
         }
